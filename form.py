553c2b9cced5cc5764800273ddd566f1c16bd764
Andres Villasmil <aevillas@uncg.edu>
2022-07-20 21:33:30 +0000

    Added Login/Signup

--- form.py
+++ form.py
@@ -10,3 +10,8 @@ class RegistrationForm(FlaskForm):
     #confirm_password = PasswordField('Confirm Password',
                                      #validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    phonenumber = StringField('Phone Number', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
--- main.py
+++ main.py
@@ -4,50 +4,40 @@ from api import *
 from user_input import *
 import datetime
 from flask import Flask, render_template, url_for, flash, redirect, request
-from form import RegistrationForm
+from form import RegistrationForm, LoginForm
 from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
 
 #Flask
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  ## add this line
 app.config['SECRET_KEY'] = '288c97cf1b11a5726d571d84ccc1f5f5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-#db = SQLAlchemy(app)
+#Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+#Connect to food database
 phone_number = "4256471907"
 database = connect_database()
 
 # model that define what will be included in the database
 # db.model is the way that shows how our database will look like
-class User():
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     phonenumber = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
     # this is a magic method 
     def __repr__(self):
-        return f"User('{self.phonenumber}' )"
-
-#Hana Register
-@app.route("/register", methods = ['GET','POST'])                          # this tells you the URL the method below is related to
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-            user = make_account(check_valid_phone(form.phonenumber.data), secure_password(form.password.data))
-            if user != False:
-                create_table(database, phone_number)
-                print(get_all_login_data())
-                flash(f'Account created for {form.phonenumber.data}!', 'success')
-                return redirect(url_for('login')) # if so - send to home page
-
-            #user = User(phonenumber=form.phonenumber.data, password=form.password.data)
-            #db.session.add(user)
-            #db.session.commit()
-            #result = request.form
-            #return render_template('user.html', result = result)
-            
-    return render_template('register.html', title='Register', form=form)        # this prints HTML to the webpage
+        return f"User('{self.phonenumber}', '{self.password}')"
 
 @app.route("/")
 @app.route("/index")
@@ -55,18 +45,58 @@ def register():
 def home():
     return render_template("index.html")
 
-@app.route("/add")
-def add():
-    return render_template("additems.html")
+#Hana Register
+@app.route("/register", methods = ['GET','POST'])                          # this tells you the URL the method below is related to
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if User.query.filter_by(phonenumber=form.phonenumber.data).first():
+            return redirect(url_for('register'))
+        else:
+            user = User(phonenumber=form.phonenumber.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash(f'Account created for {form.phonenumber.data}!', 'success')
+            return redirect(url_for('login')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form) 
 
 @app.route("/login", methods = ['GET','POST'])
 def login():
-    return render_template("login.html")
+    #https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
+    form = LoginForm()
+    if form.validate_on_submit():
+        #Get Data
+        phonenumber = form.phonenumber.data
+        password = form.password.data
+
+        #Confirm data
+        check_user = User.query.filter_by(phonenumber=phonenumber).first()
+        if not check_user or not check_user.password == password:
+            flash('Login credentials incorrect, please try again')
+            return redirect(url_for('login')) 
+        else:
+            login_user(check_user, remember=True)
+            return redirect(url_for('dashboard'))
+
+
+    return render_template("login.html", title = "Log-In", form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route("/add")
+@login_required
+def add():    
+    return render_template("additems.html")
 
 @app.route("/dashboard", methods=['GET', 'POST'])
+@login_required
 def dashboard():
     if phone_number != "":
-        data = database_to_json(database, phone_number)#'[[4, "2020-10-05 00:00:00", "Oranges"], [8, "2022-07-19 00:00:00", "Apples"], [9, "2022-07-23 00:00:00", "Beef"]]'
+        data = database_to_json(database, current_user.phonenumber)#'[[4, "2020-10-05 00:00:00", "Oranges"], [8, "2022-07-19 00:00:00", "Apples"], [9, "2022-07-23 00:00:00", "Beef"]]'
         return render_template("dashboard.html", data=data)
     else:
         return redirect(url_for('login'))
